b0608e571664e7205e6ef18211922064e866d6ed
Wade <wadehuang36@gmail.com>
2015-10-16 17:12:02 -0400

    Add intergate TopMenu

--- RightMeowMeow/CustomViewController.swift
+++ RightMeowMeow/CustomViewController.swift
@@ -8,93 +8,59 @@
 
 import UIKit
 
-class CustomViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource, UICollectionViewDelegate{
-    
+class CustomViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource, UICollectionViewDelegate {
+    static let screenWidth: CGFloat = 1920
+    static let screenHeight: CGFloat = 850
+
     var collectionView: UICollectionView!
-    
-    
+    var topMenuView: TopMenuView?;
     var cats = [Entry]()
-    let catApiURL = "https://www.reddit.com/r/cats/hot.json?limit=100"
-    
-    var index = 0
-    var afterKey = ""
-    var firstImageView : UIImageView?
-    static let screenWidth :CGFloat = 1920
-    static let screenHeight :CGFloat = 850
-    
-    
-    
-    
-    
-    func test(){
-        print("called -> \(index)")
-        
-        
-        if let URL = NSURL(string:self.cats[index++].ImgUrl){
-            
-            if let data = NSData(contentsOfURL: URL){
+    var imageIndex = 0
+    var firstImageView: UIImageView?
+
+    func loadImage() {
+        print("loadCatImage -> \(imageIndex)")
+        if let URL = NSURL(string: self.cats[imageIndex++].ImgUrl) {
+            if let data = NSData(contentsOfURL: URL) {
 //                if index > self.cats.count-10{
 //                    EntryService.FetchMoreAsnyc({
-//                        
 //                        entries in
 //                        for entry in entries{
 //                            self.cats.append(entry)
 //                            print(entry.ImgUrl)
 //                            print("fetching more")
 //                        }
-//                        
-//                        
-//                        
 //                    })
-//                    
-                
 //                }
+                
                 var timeDuration = (UIImage.animatedImageWithData(data)?.duration)!
                 self.imageSwitch(self.firstImageView!, image: UIImage.animatedImageWithData(data)!, duration: timeDuration)
-                
-                
-                
             }
         }
-        
-        
+
         /*
-        if let URL = NSURL(string:self.cats[index++].ImgUrl){
-        
-        if let data = NSData(contentsOfURL: URL){
-        print("called2")
-        
-        //let timeDuration = UIImage.animatedImageWithData(data)?.duration
-        
-        
-        
-        
-        
-        //UIImage.duration(data)
-        
-        
-        
-        //self.imageFadeIn(self.firstImageView!, image: UIImage(data:data)!)
-        //var timer = NSTimer.scheduledTimerWithTimeInterval((UIImage.animatedImageWithData(data)?.duration)!, target: self, selector: "scrollView", userInfo: nil, repeats: true)
-        
-        
-        
-        
-        }
-        
-        
-        }
-        
-        
+            if let URL = NSURL(string:self.cats[index++].ImgUrl){
+
+            if let data = NSData(contentsOfURL: URL){
+            print("called2")
+
+            //let timeDuration = UIImage.animatedImageWithData(data)?.duration
+            //UIImage.duration(data)
+            //self.imageFadeIn(self.firstImageView!, image: UIImage(data:data)!)
+            //var timer = NSTimer.scheduledTimerWithTimeInterval((UIImage.animatedImageWithData(data)?.duration)!, target: self, selector: "scrollView", userInfo: nil, repeats: true)
+            }
+            }
         */
-        
-        
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+
+        topMenuView = TopMenuView();
+        topMenuView!.ReportHandler = self.HandleReport
+
+        self.view.addSubview(topMenuView!)
+
         let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
         layout.sectionInset = UIEdgeInsets(top: 0, left: 20.0, bottom: 0, right: 20.0)
         layout.itemSize = CGSize(width: 360, height: 230)
@@ -102,41 +68,22 @@ class CustomViewController: UIViewController, UICollectionViewDelegateFlowLayout
         layout.minimumLineSpacing = 30.0;
         layout.minimumInteritemSpacing = 30.0;
         layout.scrollDirection = UICollectionViewScrollDirection.Horizontal
-        
-        
-        collectionView = UICollectionView(frame:CGRectMake(0,825,1920,230), collectionViewLayout: layout)
-        
+
+
+        collectionView = UICollectionView(frame: CGRectMake(0, 825, 1920, 230), collectionViewLayout: layout)
         collectionView.dataSource = self
         collectionView.delegate = self
         collectionView.scrollEnabled = false
-        
-        
-        
-        
-    
-
-
-        
-        
-        
-        
-        
-        
-        collectionView.registerClass(CatTrayViewCell.self, forCellWithReuseIdentifier:"Cell")
+        collectionView.registerClass(CatTrayViewCell.self, forCellWithReuseIdentifier: "Cell")
         self.view.addSubview(collectionView)
+
         let leftswipeRecognizer = UISwipeGestureRecognizer(target: self, action: "leftSwipe")
         leftswipeRecognizer.direction = .Left
         self.view.addGestureRecognizer(leftswipeRecognizer)
         let rightswipeRecognizer = UISwipeGestureRecognizer(target: self, action: "rightSwipe")
         rightswipeRecognizer.direction = .Right
         self.view.addGestureRecognizer(rightswipeRecognizer)
-        
-        
-        
-        
-        
-        
-        
+
         /*
         let firstImageView = UIImageView(image:UIImage(named:"cat"))
         firstImageView.frame = CGRectMake(582, 162, 756, 756)
@@ -144,70 +91,37 @@ class CustomViewController: UIViewController, UICollectionViewDelegateFlowLayout
         
         self.imageFadeIn(firstImageView, image: UIImage(named:"cat2")!)
         */
-        EntryService.FetchAsnyc{
-            
+        EntryService.FetchAsnyc {
             entries in
-            for entry in entries{
+            for entry in entries {
                 self.cats.append(entry)
                 print(entry.ImgUrl)
             }
-            
-            
-            
+
             dispatch_async(dispatch_get_main_queue()) {
-                self.firstImageView = UIImageView(image:UIImage(named:"cat"))
+                self.firstImageView = UIImageView(image: UIImage(named: "cat"))
                 self.firstImageView!.frame = CGRectMake(0, 0, 1920, 800)
                 self.firstImageView!.contentMode = UIViewContentMode.ScaleAspectFit
                 self.firstImageView!.clipsToBounds = true
-                
-                
-                
                 self.view.addSubview(self.firstImageView!)
-                
+
                 // update some UI
                 self.collectionView.reloadData()
-                
-                self.test()
-                
-                
-                
+
+                self.loadImage()
             }
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
         }
-        
-        
-        
-        
-        
+
         //collectionView.backgroundColor = UIColor(patternImage: UIImage(named:"background")!)
-        
-        
-        
-        
         // Do any additional setup after loading the view, typically from a nib.
     }
-    func imageSwitchAgain(imageView: UIImageView, image:UIImage){
-        
+
+    func imageSwitchAgain(imageView: UIImageView, image: UIImage) {
         imageView.image = image
         scrollView(UICollectionViewScrollPosition.Left)
-        
-        
     }
-    
-    func imageSwitch(imageView: UIImageView, image: UIImage, duration: NSTimeInterval){
+
+    func imageSwitch(imageView: UIImageView, image: UIImage, duration: NSTimeInterval) {
         /*let secondImageView = UIImageView(image: image)
         secondImageView.frame = CGRectMake(0,0,1920,800)
         secondImageView.contentMode = UIViewContentMode.ScaleAspectFit
@@ -217,13 +131,13 @@ class CustomViewController: UIViewController, UICollectionViewDelegateFlowLayout
         */
         self.firstImageView?.image = image
         self.scrollView(UICollectionViewScrollPosition.Left)
-        
+
         //secondImageView.removeFromSuperview()
-        
-        var timer = NSTimer.scheduledTimerWithTimeInterval(duration, target: self, selector: "test", userInfo: nil, repeats: false)
+
+        var timer = NSTimer.scheduledTimerWithTimeInterval(duration, target: self, selector: "loadImage", userInfo: nil, repeats: false)
     }
-    
-    func imageFadeIn(imageView: UIImageView, image: UIImage){
+
+    func imageFadeIn(imageView: UIImageView, image: UIImage) {
         imageView.alpha = 1.0
         let secondImageView = UIImageView(image: image)
         secondImageView.frame = CGRectMake(0, 0, 1920, 800)
@@ -231,126 +145,124 @@ class CustomViewController: UIViewController, UICollectionViewDelegateFlowLayout
         secondImageView.alpha = 1.0
         secondImageView.contentMode = UIViewContentMode.ScaleAspectFit
         secondImageView.clipsToBounds = true
-        
-        
+
+
         self.view.insertSubview(secondImageView, aboveSubview: imageView)
-        
+
         UIView.animateWithDuration(2.0, delay: 0.0, options: .CurveEaseOut, animations: {
             //imageView.frame = CGRectMake(448, 28, 1024, 1024)
-            
-            
-            }, completion: {_ in
+
+
+        }, completion: {
+            _ in
+
+
+            UIView.animateWithDuration(2.0, delay: 0.0, options: .CurveEaseOut, animations: {
+                imageView.frame = CGRectMake(0, 0, 1920, 800)
+
+                secondImageView.frame = CGRectMake(0, 0, 1920, 800)
+
+
+            }, completion: {
+                _ in
+                //imageView.frame = CGRectMake(582, 162, 756, 756)
+                //print(secondImageView.alpha)
+                //print(imageView.alpha)
+
+
+                secondImageView.removeFromSuperview()
                 
-                
-                UIView.animateWithDuration(2.0, delay: 0.0, options: .CurveEaseOut, animations: {
-                    imageView.frame = CGRectMake(0, 0, 1920, 800)
-                    
-                    secondImageView.frame = CGRectMake(0,0,1920,800)
-                    
-                    
-                    
-                    }, completion: {_ in
-                        //imageView.frame = CGRectMake(582, 162, 756, 756)
-                        //print(secondImageView.alpha)
-                        //print(imageView.alpha)
-                        
-                        
-                        secondImageView.removeFromSuperview()
-                        self.test()
-                        
-                        //self.view.addSubview(imageView)
-                })
-                
+                self.loadImage()
+
+                //self.view.addSubview(imageView)
+            })
         })
-        
     }
-    
+
     override func viewDidAppear(animated: Bool) {
-        
-        
-        
+
+
     }
-    func rightSwipe(){
+
+    func rightSwipe() {
         scrollView(UICollectionViewScrollPosition.Right)
         print("called")
         scrollView(.Right)
     }
-    func leftSwipe(){
+
+    func leftSwipe() {
         print("called")
         scrollView(UICollectionViewScrollPosition.Left)
         scrollView(.Left)
-        
+
     }
-    func scrollView(var direction: UICollectionViewScrollPosition){
+
+    func scrollView(var direction: UICollectionViewScrollPosition) {
         //print(self.collectionView.indexPathsForVisibleItems())
-        
-        var visibleItems =  []
-        
-        if visibleItems.count != 0{
-                        print("called3 for index \(index) ")
-                        var currentItem : NSIndexPath = visibleItems.objectAtIndex(0) as! NSIndexPath
-                        print("was visible -> \(self.cats[currentItem.row].ImgUrl)")
-                        var nextItem = NSIndexPath(forItem: currentItem.item + 1, inSection: 0)
-                        print("now visible -> \(self.cats[nextItem.row].ImgUrl)")
-            
-                        //self.collectionView.scrollToItemAtIndexPath(visibleItems.objectAtIndex(1) as! NSIndexPath, atScrollPosition: UICollectionViewScrollPosition.Left, animated: true)
-            if direction == UICollectionViewScrollPosition.Left{
+
+        var visibleItems = []
+
+        if visibleItems.count != 0 {
+            print("called3 for index \(imageIndex) ")
+            var currentItem: NSIndexPath = visibleItems.objectAtIndex(0) as! NSIndexPath
+            print("was visible -> \(self.cats[currentItem.row].ImgUrl)")
+            var nextItem = NSIndexPath(forItem: currentItem.item + 1, inSection: 0)
+            print("now visible -> \(self.cats[nextItem.row].ImgUrl)")
+
+            //self.collectionView.scrollToItemAtIndexPath(visibleItems.objectAtIndex(1) as! NSIndexPath, atScrollPosition: UICollectionViewScrollPosition.Left, animated: true)
+            if direction == UICollectionViewScrollPosition.Left {
                 self.collectionView.scrollToItemAtIndexPath(nextItem, atScrollPosition: UICollectionViewScrollPosition.Left, animated: true)
                 print("called left")
-                
-            }else if direction == UICollectionViewScrollPosition.Right{
+
+            } else if direction == UICollectionViewScrollPosition.Right {
                 self.collectionView.scrollToItemAtIndexPath(nextItem, atScrollPosition: UICollectionViewScrollPosition.Right, animated: true)
                 print("called right")
-                
+
             }
-            
-            
-            
 //            if (self.cats.count > (index+1) )
 //            {
 //                self.collectionView.scrollToItemAtIndexPath(NSIndexPath( index: index+1),atScrollPosition: UICollectionViewScrollPosition.Left, animated: false);
 //            }
-            
         }
-        
     }
-    
+
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return cats.count
     }
-    
+
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as! CatTrayViewCell
         //cell.imageCat.image = UIImage(named:"cat")
-        if let URL = NSURL(string:cats[indexPath.row+1].ImgUrl){
-            if let data = NSData(contentsOfURL: URL){
+        if let URL = NSURL(string: cats[indexPath.row + 1].ImgUrl) {
+            if let data = NSData(contentsOfURL: URL) {
                 //cell.imageCat.image = UIImage(data:data)
-                if URL.pathExtension!.lowercaseString == "gif"{
+                if URL.pathExtension!.lowercaseString == "gif" {
                     //cell.imageCat.image = UIImage.animatedImageWithData(data)
-                    
+
                     cell.imageCat.image = UIImage.animatedImageWithData(data)
-                }else{
-                    cell.imageCat.image = UIImage(data:data)
+                } else {
+                    cell.imageCat.image = UIImage(data: data)
                 }
                 //let catImage = UIImage.animatedImageWithData(data)
                 //cell.imageCat.image = UIImage.animatedImageWithData(data)
-                
-                
-                
             }
-            
-            
         }
-        
-        
-        
+
         cell.backgroundColor = UIColor.orangeColor()
-        
+
         return cell
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    func HandleReport() {
+        if (imageIndex >= 0 && imageIndex < cats.count) {
+            self.cats.removeAtIndex(imageIndex);
+            // UI Update
+            self.collectionView.reloadData()
+        }
     }
 }
