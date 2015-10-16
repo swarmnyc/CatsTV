6b7b2607a957e85b56926b23fb45975524d76591
Wade <wadehuang36@gmail.com>
2015-10-16 16:44:15 -0400

    Add TopMenu

---
+++ RightMeowMeow/TestViewController.swift
@@ -0,0 +1,130 @@
+//
+//  CarouselViewController.swift
+//  RightMeowMeow
+//
+//  Created by Johann Kerr on 10/6/15.
+//  Copyright © 2015 Johann Kerr. All rights reserved.
+//
+
+import UIKit
+
+class CarouselViewController: UIViewController, iCarouselDataSource, iCarouselDelegate {
+    let carousel = iCarousel()
+    var i = 0
+    
+    var cats = [Entry]()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.addSubview(TopMenuView())
+        
+        
+        //self.view.backgroundColor = UIColor.whiteColor()
+        
+//        EntryService.FetchAsnyc{ entries in
+//            for entry in entries{
+//                self.cats.append(entry)
+//            }
+//            
+//            self.carousel.reloadData()
+//            
+//            //testing
+////            EntryService.FetchMoreAsnyc{entries in
+////                for entry in entries{
+////                    self.cats.append(entry)
+////                }
+////                
+////                self.carousel.reloadData()
+////
+////                EntryService.FetchMoreAsnyc{entries in
+////                    for entry in entries{
+////                        self.cats.append(entry)
+////                    }
+////                    
+////                    self.carousel.reloadData()                    
+////                }
+////            }
+//        }
+
+        
+        //let catRect:CGRect = CGRectMake(100, 100, self.view.frame.width/2, self.view.frame.height/2)
+        
+        self.carousel.frame = self.view.frame
+        self.carousel.type = .CoverFlow2
+        self.carousel.delegate = self
+        self.carousel.dataSource = self
+        
+        self.view.addSubview(carousel)
+        print("carousel added")
+        
+        var timer = NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, selector: "update", userInfo: nil, repeats: true)
+        
+        
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func numberOfItemsInCarousel(carousel: iCarousel) -> Int
+    {
+        return cats.count
+    }
+    
+    func carousel(carousel: iCarousel, viewForItemAtIndex index: Int, reusingView view: UIView?) -> UIView
+    {
+        
+        var catView : UIImageView
+        
+        catView = UIImageView(frame: self.view.frame)
+        catView.contentMode = UIViewContentMode.ScaleAspectFit
+        catView.clipsToBounds = true
+        if let URL = NSURL(string:cats[index].ImgUrl){
+            if let data = NSData(contentsOfURL: URL){
+                //catView.image = UIImage(data:data)
+                
+                
+                
+                if URL.pathExtension!.lowercaseString == "gif"{
+                    catView.image = UIImage.animatedImageWithData(data)
+                }else{
+                    catView.image = UIImage(data:data)
+                }
+                
+                 //cell.imageCat.image = UIImage.animatedImageWithData(data)
+                
+            }
+            
+        }
+        
+        
+        return catView
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    func update(){
+        
+        //self.carousel.scrollByNumberOfItems(0, duration:3.0 )
+        self.carousel.scrollToItemAtIndex(self.i, animated: true)
+        
+        
+        self.i++
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RightMeowMeow/TopMenuView.swift
@@ -0,0 +1,9 @@
+//
+//  TopMenuView.swift
+//  RightMeowMeow
+//
+//  Created by SWARMMAC01 on 10/16/15.
+//  Copyright © 2015 Johann Kerr. All rights reserved.
+//
+
+import Foundation
